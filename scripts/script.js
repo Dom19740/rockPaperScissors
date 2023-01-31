6ae0cdf74d0f3117931ea933a11e738f5f017b41
Dominic Buzugbe <120669022+Dom19740@users.noreply.github.com>
2023-01-31 11:41:09 +0000

    Write initial pseudocode from the instructions.

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+function getComputerChoice(){
+
+}
+
+//Call function getComputerChoice
+    //randomly return computerSelection: either Rock Paper or Scissors
+    //console.log(computerSelection value)
+
+//Get function getplayerChoice
+    //user prompt for Rock Paper or Scissors (case insensitive)
+    //return playerSelection
+    //console.log(playerSelection value)
+
+//function to play 1 round
+function playRound(playerSelection, conputerSelection){
+    //take 2 parameters playerSelection and computerSelection
+    //RETURN a string that declares winner
+        //"You Lose! Rock beats Scissors"
+        //"You Lose! Paper beats Rock"
+        //"You Lose! Scissors beats Paper"
+        //"You Win! Rock beats Scissors"
+        //"You Win! Paper beats Rock"
+        //"You Win! Scissors beats Paper"
+        //console.log(return string7)
+
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+//Write function called game to call playRound
+    //play 5 rounds and keep score 
+    for (let i = 0; i < 5; i++){
+
+
+    }
+    //return a winner/loser
+    //console.log(winner/loser)
